8e75e6023b1578063f590ab0865b1d9f22cefb19
Harper Frankstone <Harper Frankstone>
2017-02-11 13:53:14 -0600

    set up file structure and rough beginnings of the IIFEs

---
+++ js/donorInfo.js
@@ -0,0 +1,11 @@
+"use strict";
+//this iife will hold the functions that are responsible for holding the donor info in a private array
+var Donor = (function(moneyDonor){
+	var info = {};
+
+	//the donor info function will go here; this will be the getInfo function, getting the info from 
+
+
+	//the addDonor method will be here and this will be how the info gets put into the info object. 
+
+})(Donor || {});
---
+++ js/switchBoard.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var Donor = (function(moneyDonor){
+	// var info = {}; don't think that I need this here, but I'll keep it until I'm sure
+
+	
+
+})(Donor || {});
