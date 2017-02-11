fbae8ab3b7f9d1f2d4109f3d0f6e88188b4f59bc
Harper Frankstone <Harper Frankstone>
2017-02-11 16:31:49 -0600

    added a couple more variables for event listeners, working on the planning before coding more

--- js/switchBoard.js
+++ js/switchBoard.js
@@ -2,7 +2,9 @@
 
 var Donor = (function(moneyDonor){
 	// var info = {}; don't think that I need this here, but I'll keep it until I'm sure
-	
+	//variables declared to be used for the event listeners
+	var donateBtn = document.getElementById('donate');
+	var cancelBtn = document.getElementById('cancel');
 	
 	
 
