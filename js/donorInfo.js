577b5dba35c25d1e0637362d433679944313b465
Harper Frankstone <Harper Frankstone>
2017-02-23 11:12:32 -0600

    met mvp and everything is working like I want it to, may not be the cleanest way to go about it though

--- js/donorInfo.js
+++ js/donorInfo.js
@@ -5,8 +5,19 @@ var Donor = (function(moneyDonor){
 
 	return{
 	//the donor info function will go here; this will be the getInfo function, getting the info from the input forms
-	getInfo: function(info){
+	buildDonorTable: function(info){
 		console.log("info object from the donorInfo", info);
+		var table = document.getElementById('table-body');
+		var newRow = table.insertRow(0);
+		var cell1 = newRow.insertCell(0);
+		var cell2 = newRow.insertCell(1);
+		var cell3 = newRow.insertCell(2);
+		var cell4 = newRow.insertCell(3);
+		cell1.innerHTML = info.name;
+		cell2.innerHTML = info.email;
+		cell3.innerHTML = info.pledge;
+		cell4.innerHTML = info.donationType;
+
 	}
 
 	//the addDonor method will be here and this will be how the info gets put into the info object. 
--- js/switchBoard.js
+++ js/switchBoard.js
@@ -25,18 +25,31 @@ var Donor = (function(moneyDonor){
 			var inputName = document.getElementById('inputName').value;
 			var inputEmail = document.getElementById('inputEmail').value;
 			var inputDonation = parseInt(document.getElementById('inputDonation').value);
+			var donationType;
+			var lumpSum = document.getElementById('gridRadios1');
+			var perLap = document.getElementById('gridRadios2');
+
+			if(lumpSum.checked){
+				donationType = "Lump Sum";
+			} else if(perLap.checked){
+				donationType = "Per Lap";
+			} else {
+				alert("Pick a donation type.");
+			}
 
 			info = {
 				name: inputName,
 				email: inputEmail,
-				pledge: inputDonation
+				pledge: "$" + inputDonation,
+				donationType: donationType
 			};
 			console.log("info object", info);
-
+			formReset();
+			document.getElementById('inputName').focus();
 
 			console.log("Form has been submitted");
-			//last thing should call the getInfo() from the donorInfo.js so that it can store that info into the info object
-	Donor.getInfo(info);
+			//last thing should call the buildDonorTable() from the donorInfo.js so that it can store that info into the info object
+	Donor.buildDonorTable(info);
 	}
 
 })(Donor || {});
