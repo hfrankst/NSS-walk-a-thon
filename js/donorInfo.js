8002aa1475a51a6486dc80cf71be35dcaa5a3c86
Harper Frankstone <Harper Frankstone>
2017-02-23 10:33:11 -0600

    got the event listeners working, and the form submitting

--- js/donorInfo.js
+++ js/donorInfo.js
@@ -1,14 +1,21 @@
 "use strict";
+console.log("donorInfo page");
 //this iife will hold the functions that are responsible for holding the donor info in a private array
 var Donor = (function(moneyDonor){
-	var info = {};
 
+	return{
 	//the donor info function will go here; this will be the getInfo function, getting the info from the input forms
-
+	getInfo: function(info){
+		console.log("info object from the donorInfo", info);
+	}
 
 	//the addDonor method will be here and this will be how the info gets put into the info object. 
+	//this function will need to grab the donorInfo and put it into a table
+
+
+	};
 
 })(Donor || {});
 
 
-moneyDonor.addDonor()
+// moneyDonor.addDonor()
--- js/switchBoard.js
+++ js/switchBoard.js
@@ -1,15 +1,42 @@
 "use strict";
-
+console.log("switchBoard page");
 var Donor = (function(moneyDonor){
-	// var info = {}; don't think that I need this here, but I'll keep it until I'm sure
+	
+
+	var info = {}; 
 	//variables declared to be used for the event listeners
 	var donateBtn = document.getElementById('donate');
-	var cancelBtn = document.getElementById('cancel');//this listener needs to reset the form
+	var cancelBtn = document.getElementById('cancel');
+
 	
-	cancelBtn.addEventListener('click', function(){
-		document.getElementById('myForm').reset();
-	});
+	//event listener for the cancel button
+	cancelBtn.addEventListener('click', formReset); 
+
+		function formReset () {
+			document.getElementById('myForm').reset();
+			console.log("Form has been reset");	
+		}
+	
 	//the donate button needs to run the getInfo() function from the donorInfo page
-	
+	donateBtn.addEventListener('click', submitDonation);
+
+		function submitDonation (){
+			//variables for each part of the form
+			var inputName = document.getElementById('inputName').value;
+			var inputEmail = document.getElementById('inputEmail').value;
+			var inputDonation = parseInt(document.getElementById('inputDonation').value);
+
+			info = {
+				name: inputName,
+				email: inputEmail,
+				pledge: inputDonation
+			};
+			console.log("info object", info);
+
+
+			console.log("Form has been submitted");
+			//last thing should call the getInfo() from the donorInfo.js so that it can store that info into the info object
+	Donor.getInfo(info);
+	}
 
 })(Donor || {});
